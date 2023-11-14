e4ac22a63bd4df5d82d76887df6a1b0d6281d425
joseftw <josef@localhost.local>
2023-11-14 19:47:24 +0100

    Adds ExampleConsumer

---
+++ test/ExampleConsumer/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using JOS.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+var configuration = new ConfigurationBuilder().AddInMemoryCollection(new List<KeyValuePair<string, string?>>
+{
+    new("SomeKey", "SomeValue")
+}).Build();
+
+var value = configuration.GetRequiredValue<string>("SomeKey");
+Console.WriteLine(value);
